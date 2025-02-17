32172de68f45be7a1daa5f28c67acc31397dc567
João Bartolot <jvcbartolot@gmail.com>
2025-02-17 15:15:55 -0300

    Fix navbar

--- src/layout/Navbar.tsx
+++ src/layout/Navbar.tsx
@@ -1,3 +1,4 @@
+import { motion } from 'framer-motion'
 import { useEffect, useRef, useState } from 'react'
 import { twJoin } from 'tailwind-merge'
 import Logo from '../assets/images/logo.svg?react'
@@ -8,7 +9,36 @@ const offset = 80
 
 const Navbar = () => {
 	const [isOpen, setIsOpen] = useState(false)
+	const [frostHeader, setFrostHeader] = useState(false)
 	const headerRef = useRef<HTMLElement>(null)
+	const [hideFast, setHideFast] = useState(false)
+
+	// Update offset based on screen width
+	useEffect(() => {
+		const updateOffset = () => {
+			const md = window.matchMedia('(min-width: 768px)').matches
+			if (md) {
+				setIsOpen(false)
+				setHideFast(false)
+				return
+			}
+			setHideFast(true)
+		}
+
+		updateOffset()
+		window.addEventListener('resize', updateOffset)
+
+		return () => window.removeEventListener('resize', updateOffset)
+	}, [])
+
+	useEffect(() => {
+		window.onscroll = () =>
+			window.scrollY === 0 ? setFrostHeader(false) : setFrostHeader(true)
+
+		return () => {
+			window.onscroll = null
+		}
+	}, [])
 
 	useEffect(() => {
 		if (!isOpen) return
@@ -28,6 +58,21 @@ const Navbar = () => {
 		}
 	}, [isOpen])
 
+	const topVariants = {
+		closed: { rotate: 0, y: 0 },
+		open: { rotate: 45, y: 8 },
+	}
+
+	const middleVariants = {
+		closed: { opacity: 1 },
+		open: { opacity: 0 },
+	}
+
+	const bottomVariants = {
+		closed: { rotate: 0, y: 0 },
+		open: { rotate: -45, y: -8, width: '100%' },
+	}
+
 	const scrollToElement = (sectionId: string) => {
 		const sectionElement = document.getElementById(sectionId)
 		if (sectionElement) {
@@ -46,16 +91,19 @@ const Navbar = () => {
 		event: React.MouseEvent<HTMLAnchorElement>,
 		sectionId: string
 	) => {
-		event.preventDefault() // Prevent default anchor link behavior
+		event.preventDefault()
 		scrollToElement(sectionId)
 	}
 
 	return (
-		<nav
+		<header
 			ref={headerRef}
 			className={twJoin(
-				'fixed flex h-[80px] w-full items-center justify-center px-4 z-30',
-				'md:px-12'
+				'fixed flex w-full items-center justify-center h-[80px] px-4 py-4 z-30',
+				'md:px-12',
+				!isOpen && hideFast ? 'delay-300' : '',
+				isOpen && hideFast ? '' : 'transition-color duration-300',
+				frostHeader && !isOpen ? 'bg-white/5 backdrop-blur-sm' : ''
 			)}
 		>
 			<div className="relative flex w-full items-center justify-between">
@@ -68,6 +116,31 @@ const Navbar = () => {
 				>
 					<Logo className="w-24 h-fit" />
 				</div>
+				<button
+					className="z-50 flex h-6 w-8 flex-col justify-around md:hidden"
+					onClick={() => setIsOpen(!isOpen)}
+				>
+					<motion.span
+						className="h-[3px] w-full rounded-full bg-black dark:bg-white"
+						variants={topVariants}
+						animate={isOpen ? 'open' : 'closed'}
+						transition={{ duration: 0.3 }}
+					/>
+
+					<motion.span
+						className="h-[3px] w-[80%] rounded-full bg-black dark:bg-white"
+						variants={middleVariants}
+						animate={isOpen ? 'open' : 'closed'}
+						transition={{ duration: 0.3 }}
+					/>
+
+					<motion.span
+						className="h-[3px] w-[70%] rounded-full bg-black dark:bg-white"
+						variants={bottomVariants}
+						animate={isOpen ? 'open' : 'closed'}
+						transition={{ duration: 0.3 }}
+					/>
+				</button>
 
 				<div className="flex space-x-4 z-50">
 					<ThemeToggleButton />
@@ -79,7 +152,7 @@ const Navbar = () => {
 					</Button>
 				</div>
 
-				<div
+				<nav
 					className={twJoin(
 						'absolute inset-0 hidden h-full items-center justify-center space-x-12',
 						'md:flex'
@@ -103,9 +176,39 @@ const Navbar = () => {
 					>
 						Projects
 					</a>
-				</div>
+				</nav>
 			</div>
-		</nav>
+			<motion.nav
+				initial={{ x: '-100%' }} // Start completely off-screen to the left
+				animate={{ x: isOpen ? 0 : '-100%' }} // Slide in to `left-0`
+				exit={{ x: '-100%' }} // Slide out when closed
+				transition={{
+					type: 'tween',
+					duration: 0.4,
+					ease: 'easeOut',
+				}}
+				className={twJoin(
+					'md:hidden absolute top-0 left-0 overflow-hidden bg-white/5 backdrop-blur-sm',
+					'z-40 flex flex-col items-start space-y-6 py-24 pl-4 pr-24 rounded-br-[30px]'
+				)}
+			>
+				<a href="#about" onClick={e => handleAnchorClick(e, 'about')}>
+					About
+				</a>
+				<a
+					href="#experience"
+					onClick={e => handleAnchorClick(e, 'experience')}
+				>
+					Experience
+				</a>
+				<a
+					href="#projects"
+					onClick={e => handleAnchorClick(e, 'projects')}
+				>
+					Projects
+				</a>
+			</motion.nav>
+		</header>
 	)
 }
 

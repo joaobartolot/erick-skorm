788cc4f7a24e96b853d412cca5e900a15dafcaa2
João Bartolot <jvcbartolot@gmail.com>
2025-02-19 18:41:38 -0300

    Update i18n configuration to set language based on system preference or saved setting

--- src/i18n.ts
+++ src/i18n.ts
@@ -2,10 +2,13 @@ import i18n from 'i18next'
 import HttpBackend from 'i18next-http-backend'
 import { initReactI18next } from 'react-i18next'
 
+const savedLanguage = localStorage.getItem('language')
+const systemLanguage = navigator.language.startsWith('pt') ? 'pt' : 'en'
+
 i18n.use(HttpBackend)
 	.use(initReactI18next)
 	.init({
-		lng: localStorage.getItem('language') || 'en',
+		lng: savedLanguage || systemLanguage,
 		fallbackLng: 'en',
 		backend: {
 			loadPath: '/locales/{{lng}}.json',

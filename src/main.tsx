f3ac3a6df6edbe32bf7d460a69f6072adb0df1c1
João Bartolot <jvcbartolot@gmail.com>
2025-02-20 21:42:50 -0300

    Integrate Vercel Analytics for enhanced performance tracking

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,13 @@
+import { inject } from '@vercel/analytics'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './i18n.ts'
 import './index.css'
+
+if (import.meta.env.PROD) {
+	inject()
+}
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
